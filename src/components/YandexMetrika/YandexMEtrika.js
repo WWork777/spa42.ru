25c45f784408266e47c808346ebe5a0110ebc370
unknown <parsikovevgenij470@gmail.com>
2025-11-01 13:55:35 +0700

    fix

--- src/components/YandexMetrika/YandexMEtrika.js
+++ src/components/YandexMetrika/YandexMEtrika.js
@@ -22,7 +22,7 @@ export default function YandexMetrika() {
         a.parentNode.insertBefore(k, a);
     })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(104039614, "init", {
+    ym(105061028, "init", {
       clickmap: true,
       trackLinks: true,
       accurateTrackBounce: true,
@@ -34,7 +34,7 @@ export default function YandexMetrika() {
     <noscript>
       <div>
         <img
-          src="https://mc.yandex.ru/watch/104039614"
+          src="https://mc.yandex.ru/watch/105061028"
           style={{ position: "absolute", left: "-9999px" }}
           alt=""
         />
